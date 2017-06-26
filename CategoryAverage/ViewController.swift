078b8763f9681908103bdcb8834a84e9b4ecd9ff
Suraj <freesuraj@gmail.com>
2017-06-26 17:56:12 +1000

    Initial Commit

---
+++ CategoryAverage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CategoryAverage
+//
+//  Created by Suraj Pathak on 26/6/17.
+//  Copyright © 2017 Suraj Pathak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
